c9e16442726fba852182107e95b76b292694fa44
Jqsm6 <jqsm6@yandex.ru>
2023-08-09 15:02:53 +0300

    Delete auth tests

--- internal/service/interface.go
+++ internal/service/interface.go
@@ -2,8 +2,6 @@ package service
 
 import "github.com/festivio/festivio-backend/domain"
 
-//go:generate mockgen -source=interface.go -destination=mocks/mock.go
-
 type ServiceInterface interface {
 	// Authorization methods
 	SignUpUser(signUpInput *domain.SignUpInput) error
